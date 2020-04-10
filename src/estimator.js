532a3cb3b8405eba3f846217228ae8893db05f26
kendevops <kendevops@gmail.com>
2020-04-10 14:13:42 +0100

    Fixed Indentation and others

--- src/estimator.js
+++ src/estimator.js
@@ -1,28 +1,28 @@
-const covid19ImpactEstimator = (data) => {
-    data = {
-        region: {
-            name: 'Africa',
-            avgAge: 19.7,
-            avgDailyIncomeInUSD: 5,
-            avgDailyIncomePopulation: 0.71
-        },
-        periodType: 'days',
-        timeToElapse: 58,
-        reportedCases: 674,
-        population: 66622705,
-        totalHospitalBeds: 1380614
-    };
-    
-    const impact = {};
-    const severImpact = {};
-    
-    impact.currentlyInfected = data.reportedCases * 10
-    
-    severImpact.currentlyInfected = data.reportedCases * 50
-    
-    impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 9
-    
-    severImpact.infectionsByRequestedTime = severImpact.currentlyInfected * 2 ** 9;
+const covid19ImpactEstimator = () => {
+  const data = {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  };
+
+  const impact = {};
+  const severImpact = {};
+
+  impact.currentlyInfected = data.reportedCases * 10;
+
+  severImpact.currentlyInfected = data.reportedCases * 50;
+
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 9;
+
+  severImpact.infectionsByRequestedTime = severImpact.currentlyInfected * 2 ** 9;
 };
 
 export default covid19ImpactEstimator;
