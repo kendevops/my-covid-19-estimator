9f6f7b208b9a94261c17e0f377db243cdfda5049
kendevops <kendevops@gmail.com>
2020-04-10 13:54:54 +0100

    Worked on challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,25 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  git data = {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  };
+
+  const impact = {};
+  const severImpact = {};
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  severImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** 9;
+  severImpact.infectionsByRequestedTime = severImpact.currentlyInfected * 2 ** 9;
+};
 
 export default covid19ImpactEstimator;
