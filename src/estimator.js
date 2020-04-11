9ef1cd493fbc5b749f765431f1466a3d0191a2ab
Kenneth Aladi <kennethmichael11@gmail.com>
2020-04-11 19:42:34 +0100

    made chenges on all the function and worked on the code

--- src/estimator.js
+++ src/estimator.js
@@ -1,42 +1,139 @@
-const covid19ImpactEstimator = () => {
-  const data = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
+const getDuration = (periodType, timeToElapse) => {
+  let duration;
+  switch (periodType) {
+    case 'days': {
+      duration = timeToElapse;
+      return duration;
+    }
+    case 'weeks': {
+      duration = 7 * timeToElapse;
+      return duration;
+    }
+    case 'months': {
+      duration = 30 * timeToElapse;
+      return duration;
+    }
+    default:
+  }
+  return duration;
+};
+
+const computeInfectedAndRequestTime = (reportedCases, factor, duration) => {
+  const currentlyInfected = reportedCases * factor;
+  const dayQuocent = parseInt(duration / 3, 10);
+  const infectionsByRequestedTime = currentlyInfected * 2 ** dayQuocent;
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime
   };
+};
 
-  const impact = {};
-  const severImpact = {};
+// challange 3 function
+const computeCasesAndDollarFlight = (
+  infectionsByRequestedTime,
+  avgDailyIncomeInUSD,
+  duration,
+  avgDailyIncomePopulation
+) => {
+  const casesForICUByRequestedTime = parseInt(
+    infectionsByRequestedTime * (5 / 100),
+    10
+  );
+  const casesForVentilatorsByRequestedTime = parseInt(
+    infectionsByRequestedTime * (2 / 100),
+    10
+  );
+  const dollarValue = (infectionsByRequestedTime
+      * avgDailyIncomePopulation
+      * avgDailyIncomeInUSD)
+    / duration;
+  const dollarsInFlight = parseInt(dollarValue, 10);
+  return {
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
 
-  impact.currentlyInfected = data.reportedCases * 10;
+const forChallangeOne = (data, duration) => {
+  const { reportedCases } = data;
+  const impact = computeInfectedAndRequestTime(reportedCases, 10, duration);
+  const severeImpact = computeInfectedAndRequestTime(
+    reportedCases,
+    50,
+    duration
+  );
+  return { impact, severeImpact };
+};
 
-  severImpact.currentlyInfected = data.reportedCases * 50;
+const computeSCandHB = (infectionsByRequestedTime, totalBeds) => {
+  const severeCasesByRequestedTime = infectionsByRequestedTime * (15 / 100);
+  const availableBeds = totalBeds * (35 / 100);
+  let hospitalBedsByRequestedTime;
+  if (severeCasesByRequestedTime > availableBeds) {
+    hospitalBedsByRequestedTime = parseInt(
+      availableBeds - severeCasesByRequestedTime,
+      10
+    );
+  } else {
+    hospitalBedsByRequestedTime = parseInt(availableBeds, 10);
+  }
+  return {
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime
+  };
+};
 
-  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** 9);
+const covid19ImpactEstimator = (data) => {
+  const {
+    periodType,
+    timeToElapse,
+    totalHospitalBeds,
+    region: { avgDailyIncomeInUSD, avgDailyIncomePopulation }
+  } = data;
+  const duration = getDuration(periodType, timeToElapse);
 
-  severImpact.infectionsByRequestedTime = severImpact.currentlyInfected * (2 ** 9);
+  // challange 1
+  let { impact, severeImpact } = forChallangeOne(data, duration);
 
-  impact.severeCasesByRequestedTime = impact.infectionsByRequestedTime * (15 / 100);
+  // challange 2
+  const forImpact = computeSCandHB(
+    impact.infectionsByRequestedTime,
+    totalHospitalBeds
+  );
+  impact.severeCasesByRequestedTime = forImpact.severeCasesByRequestedTime;
+  impact.hospitalBedsByRequestedTime = forImpact.hospitalBedsByRequestedTime;
 
-  severImpact.severeCasesByRequestedTime = severImpact.infectionsByRequestedTime * (15 / 100);
+  const forSevere = computeSCandHB(
+    severeImpact.infectionsByRequestedTime,
+    totalHospitalBeds
+  );
+  severeImpact.severeCasesByRequestedTime = forSevere.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = forSevere.hospitalBedsByRequestedTime;
 
-  const hospitalBedPercent = data.totalHospitalBeds * (35 / 100);
+  // challange 3
+  const casesAndDollarsFlightImplact = computeCasesAndDollarFlight(
+    impact.infectionsByRequestedTime,
+    avgDailyIncomeInUSD,
+    duration,
+    avgDailyIncomePopulation
+  );
 
-  const impactHospitalBeds = impact.severeCasesByRequestedTime - hospitalBedPercent;
+  const casesAndDollarsFlightSevere = computeCasesAndDollarFlight(
+    severeImpact.infectionsByRequestedTime,
+    avgDailyIncomeInUSD,
+    duration,
+    avgDailyIncomePopulation
+  );
 
-  const severImpactHospitalBeds = severImpact.severeCasesByRequestedTime - hospitalBedPercent;
+  impact = { ...impact, ...casesAndDollarsFlightImplact };
+  severeImpact = { ...severeImpact, ...casesAndDollarsFlightSevere };
 
-  impact.hospitalBedsByRequestedTime = Math.floor(impactHospitalBeds);
-
-  severImpact.hospitalBedsByRequestedTime = Math.floor(severImpactHospitalBeds);
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
