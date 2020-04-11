a24b9bfd70e59bc920c0278be8bb5fb55e5b4ea8
Kenneth Aladi <kennethmichael11@gmail.com>
2020-04-11 19:53:36 +0100

    Function call calculations sorted

--- src/estimator.js
+++ src/estimator.js
@@ -1,24 +1,4 @@
-const getDuration = (periodType, timeToElapse) => {
-  let duration;
-  switch (periodType) {
-    case 'days': {
-      duration = timeToElapse;
-      return duration;
-    }
-    case 'weeks': {
-      duration = 7 * timeToElapse;
-      return duration;
-    }
-    case 'months': {
-      duration = 30 * timeToElapse;
-      return duration;
-    }
-    default:
-  }
-  return duration;
-};
-
-const computeInfectedAndRequestTime = (reportedCases, factor, duration) => {
+const calInfectedAndRequestTime = (reportedCases, factor, duration) => {
   const currentlyInfected = reportedCases * factor;
   const dayQuocent = parseInt(duration / 3, 10);
   const infectionsByRequestedTime = currentlyInfected * 2 ** dayQuocent;
@@ -28,8 +8,53 @@ const computeInfectedAndRequestTime = (reportedCases, factor, duration) => {
   };
 };
 
+const calDuration = (periodType, timeToElapse) => {
+  let duration;
+  switch (periodType) {
+    case 'days': {
+      duration = timeToElapse;
+      return duration;
+    }
+    case 'weeks': {
+      duration = timeToElapse * 7;
+      return duration;
+    }
+    case 'months': {
+      duration = timeToElapse * 30;
+      return duration;
+    }
+    default:
+  }
+  return duration;
+};
+
+const challangeOneCal = (data, duration) => {
+  const { reportedCases } = data;
+  const impact = calInfectedAndRequestTime(reportedCases, 10, duration);
+  const severeImpact = calInfectedAndRequestTime(reportedCases, 50, duration);
+  return { impact, severeImpact };
+};
+
+const calSCandHB = (infectionsByRequestedTime, totalBeds) => {
+  const severeCasesByRequestedTime = infectionsByRequestedTime * (15 / 100);
+  const availableBeds = totalBeds * (35 / 100);
+  let hospitalBedsByRequestedTime;
+  if (severeCasesByRequestedTime > availableBeds) {
+    hospitalBedsByRequestedTime = parseInt(
+      availableBeds - severeCasesByRequestedTime,
+      10
+    );
+  } else {
+    hospitalBedsByRequestedTime = parseInt(availableBeds, 10);
+  }
+  return {
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime
+  };
+};
+
 // challange 3 function
-const computeCasesAndDollarFlight = (
+const calCasesAndDollarFlight = (
   infectionsByRequestedTime,
   avgDailyIncomeInUSD,
   duration,
@@ -55,35 +80,6 @@ const computeCasesAndDollarFlight = (
   };
 };
 
-const forChallangeOne = (data, duration) => {
-  const { reportedCases } = data;
-  const impact = computeInfectedAndRequestTime(reportedCases, 10, duration);
-  const severeImpact = computeInfectedAndRequestTime(
-    reportedCases,
-    50,
-    duration
-  );
-  return { impact, severeImpact };
-};
-
-const computeSCandHB = (infectionsByRequestedTime, totalBeds) => {
-  const severeCasesByRequestedTime = infectionsByRequestedTime * (15 / 100);
-  const availableBeds = totalBeds * (35 / 100);
-  let hospitalBedsByRequestedTime;
-  if (severeCasesByRequestedTime > availableBeds) {
-    hospitalBedsByRequestedTime = parseInt(
-      availableBeds - severeCasesByRequestedTime,
-      10
-    );
-  } else {
-    hospitalBedsByRequestedTime = parseInt(availableBeds, 10);
-  }
-  return {
-    severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
-  };
-};
-
 const covid19ImpactEstimator = (data) => {
   const {
     periodType,
@@ -91,20 +87,20 @@ const covid19ImpactEstimator = (data) => {
     totalHospitalBeds,
     region: { avgDailyIncomeInUSD, avgDailyIncomePopulation }
   } = data;
-  const duration = getDuration(periodType, timeToElapse);
+  const duration = calDuration(periodType, timeToElapse);
 
   // challange 1
-  let { impact, severeImpact } = forChallangeOne(data, duration);
+  let { impact, severeImpact } = challangeOneCal(data, duration);
 
   // challange 2
-  const forImpact = computeSCandHB(
+  const forImpact = calSCandHB(
     impact.infectionsByRequestedTime,
     totalHospitalBeds
   );
   impact.severeCasesByRequestedTime = forImpact.severeCasesByRequestedTime;
   impact.hospitalBedsByRequestedTime = forImpact.hospitalBedsByRequestedTime;
 
-  const forSevere = computeSCandHB(
+  const forSevere = calSCandHB(
     severeImpact.infectionsByRequestedTime,
     totalHospitalBeds
   );
@@ -112,14 +108,14 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.hospitalBedsByRequestedTime = forSevere.hospitalBedsByRequestedTime;
 
   // challange 3
-  const casesAndDollarsFlightImplact = computeCasesAndDollarFlight(
+  const casesAndDollarsFlightImplact = calCasesAndDollarFlight(
     impact.infectionsByRequestedTime,
     avgDailyIncomeInUSD,
     duration,
     avgDailyIncomePopulation
   );
 
-  const casesAndDollarsFlightSevere = computeCasesAndDollarFlight(
+  const casesAndDollarsFlightSevere = calCasesAndDollarFlight(
     severeImpact.infectionsByRequestedTime,
     avgDailyIncomeInUSD,
     duration,
